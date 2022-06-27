d874cb17c18b2e5dc7dba5c3234f6969941ac28a
MasterM <christianotteromarques@gmail.com>
2022-06-26 21:56:20 -0300

    Módulos em JS

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("William", 22));
+
+console.log(mostraCidade("Nicolas", "Rio de Janeiro"));
+
+console.log(mostraHobby("Master", "Jogar"));
